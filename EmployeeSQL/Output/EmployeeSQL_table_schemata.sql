7599bbb61a1fdc8565caa37d450acee119ad9c69
Neena Mani <neena.mani@gmail.com>
2021-07-12 14:47:44 +1000

    final

--- EmployeeSQL/Output/EmployeeSQL_table_schemata.sql
+++ EmployeeSQL/Output/EmployeeSQL_table_schemata.sql
@@ -1,17 +1,17 @@
 CREATE TABLE departments (
-dept_no VARCHAR(5) NOT NULL,
-dept_name VARCHAR(30) NOT NULL,
+dept_no VARCHAR(5),
+dept_name VARCHAR(30),
 PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE titles (
-title_id VARCHAR(6) NOT NULL,
+title_id VARCHAR(6),
 title VARCHAR(30) NOT NULL,
 PRIMARY KEY (title_id)
 );
 
 CREATE TABLE employees (
-emp_no INT NOT NULL,
+emp_no INT,
 emp_title_id VARCHAR(6) NOT NULL,
 birth_date VARCHAR(10) NOT NULL,
 first_name VARCHAR(30) NOT NULL,
@@ -31,8 +31,8 @@ FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR (5) NOT NULL,
-emp_no INT NOT NULL,
+dept_no VARCHAR (5),
+emp_no INT,
 PRIMARY KEY (dept_no, emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
@@ -40,7 +40,7 @@ FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 
 CREATE TABLE salaries (
 emp_no INT NOT NULL,
-salary INT NOT NULL,
+salary INT,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
